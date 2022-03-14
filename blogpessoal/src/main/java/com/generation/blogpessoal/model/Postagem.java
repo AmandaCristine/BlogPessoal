0ad0f8bb506e365268cdc26adc655ab9f4d77e65
AmandaCristine <amandacris.sb@gmail.com>
2022-03-14 18:56:53 -0300

    Adiconando blog pra deploy

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -2,39 +2,53 @@ package com.generation.blogpessoal.model;
 
 import java.time.LocalDateTime;
 
-
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
 @Entity
-@Table(name = "tb_postagens")
+@Table(name = "tb_postagens") 
 public class Postagem {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@NotBlank(message = "O atributo título é obrigatório.")
-	@Size(min = 5,max = 100, message = "O atributo título deve conter no mínimo 5 e no máximo 100 caracteres.")
-	private String titulo;
-	
-	@NotBlank(message = "O atributo texto é obrigatório.")
-	@Size(min = 100,max = 1000, message = "O atributo texto deve conter no mínimo 5 e no máximo 100 caracteres.")
-	private String texto;
-	
-	
-	@UpdateTimestamp	
-	private LocalDateTime data;
 
 	
-	/*Get e Set*/
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id; 
+
+	
+	@NotBlank(message = "O atributo título é Obrigatório e não pode utilizar espaços em branco!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo; 
+
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 500 caracteres")
+	private String texto;
+
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	@ManyToOne(fetch = FetchType.EAGER, optional = true)
+	@JoinColumn(name = "tema_id")
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	/* |==| Getters and Setters |==|*/
 	
 	public Long getId() {
 		return id;
@@ -67,5 +81,23 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
+
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+
 }
